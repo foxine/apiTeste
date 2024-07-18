68c9cbef5e3a1894b55b864fd33ace93aef97764
foxine <c.gc14@hotmail.com>
2024-07-18 14:23:44 -0300

    teste

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CepController;
+
+Route::get('/search/local/{ceps}', [CepController::class, 'search']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
